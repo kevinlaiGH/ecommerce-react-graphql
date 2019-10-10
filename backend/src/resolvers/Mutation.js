cb954f5df899e37d4dcbf1a18bcc3e290688573d
Kevin Lai <kevin.km.lai@gmail.com>
2019-10-10 15:49:04 +1100

    editing items sith queries and mutation

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,5 +1,7 @@
 const Mutations = {
-  async createItem(parents, args, ctx, info) {
+  async createItem(parent, args, ctx, info) {
+    // TODO: Check if they are logged in
+
     const item = await ctx.db.mutation.createItem(
       {
         data: {
@@ -8,15 +10,27 @@ const Mutations = {
       },
       info
     );
-    return item;
-  }
 
-  // createDog(parent, args, ctx, info) {
-  //   global.dogs = global.dogs || [];
-  //   const newDog = { name: args.name };
-  //   global.dogs.push(newDog);
-  //   return newDog;
-  // }
+    console.log(item);
+
+    return item;
+  },
+  updateItem(parent, args, ctx, info) {
+    // first take a copy of the updates
+    const updates = { ...args };
+    // remove the ID from the updates
+    delete updates.id;
+    // run the update method
+    return ctx.db.mutation.updateItem(
+      {
+        data: updates,
+        where: {
+          id: args.id
+        }
+      },
+      info
+    );
+  }
 };
 
 module.exports = Mutations;
--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -1,21 +1,8 @@
-// const Query = {
-//   // dogs(parent, args, ctx, info) {
-//   //   global.dogs = global.dogs || [];
-//   //   return global.dogs;
-//   // }
-
-//   async item(parents, args, ctx, info) {
-//     const items = await ctx.db.query.items();
-//     return items;
-//   }
-// };
-
-// module.exports = Query;
-
 const { forwardTo } = require("prisma-binding");
 
 const Query = {
-  items: forwardTo("db")
+  items: forwardTo("db"),
+  item: forwardTo("db")
   // async items(parent, args, ctx, info) {
   //   console.log('Getting Items!!');
   //   const items = await ctx.db.query.items();
